3c6b6e30b626bfb1c20018a169cad8817f33ace8
IanS527 <ian.singson@students.sacs.k12.in.us>
2015-12-17 09:45:39 -0500

    work
    I added team

---
+++ 1.3.9 work.py
@@ -0,0 +1,14 @@
+Team Ian:
+    if getting_team_name:
+        return 'Team Ian'
+        else:
+            if len (opponent_history)==0:
+                return 'c'
+                if history [-1]=='c':
+                    if opponent-history[-1]=='c':
+                        return 'c'
+                        else:
+                            return 'b'
+                            if history[-1]=='b':
+                                return 'b'
+                    
